a4c6e9605d0c0bf22d7d54544c64cd2ecb6ecf0d
deasa <44423343+deasa@users.noreply.github.com>
2019-07-25 23:10:58 -0600

    Add project files.

---
+++ BankingSite.UnitTests/CreditCardApplicationScorerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BankingSite.UnitTests
+{
+    [TestFixture]
+    public class CreditCardApplicationScorerTests
+    {
+        [Test]
+        public void ShouldDeclineUnderageApplicant()
+        {
+
+        }
+
+        [Test]
+        public void ShouldAskGatewayForCreditCheck()
+        {
+
+        }
+
+        [Test]
+        public void ShouldAcceptCorrectAgedApplicantWithGoodCreditHistory()
+        {
+
+        }
+    }
+}
---
+++ PsFirstNUnitUnitTests/AirlineMembershipNumberValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PsFirstNUnit;
+
+namespace PsFirstNUnitUnitTests
+{
+    [TestFixture]
+    public class AirlineMembershipNumberValidatorTests
+    {
+        [Test]
+        public void ShouldValidateCorrectNumber()
+        {
+            var sut = new AirlineMembershipNumberValidator();
+            Assert.That(sut.ValidityOf("A1234567"), Is.True);
+        }
+
+        [Test]
+        public void ShouldInvalidateIncorrectNumber()
+        {
+            var sut = new AirlineMembershipNumberValidator();
+            Assert.That(sut.ValidityOf("A123456"), Is.False);
+        }
+    }
+}
